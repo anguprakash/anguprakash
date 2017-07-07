df672fbdd81c4dcc83cf897b0588688ef795b388
anguprakash <anguprakash0005@gmail.com>
2017-07-07 11:14:06 +0530

    Update and rename Positive.java to Even.java

---
+++ Even.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Even{
+ public static void main(String args[])
+ {
+  int a;
+  Scanner s=new Scanner(System.in);
+  a=s.nextInt();
+  if(a%2==0)
+  {
+    System.out.println("even");
+    }
+    else
+    {
+    System.out.println("odd");
+    }
+      }
+      }
