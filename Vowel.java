7c5ade62d325aea685d3391b451228ac195f3780
anguprakash <anguprakash0005@gmail.com>
2017-07-07 11:50:16 +0530

    Update and rename Positive.java to Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Vowel{
+ public static void main(String args[])
+ {
+  char n;
+  Scanner s=new Scanner(System.in);
+  n=s.next().charAt(0);
+  if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
+  {
+    System.out.println("vowels");
+    }
+    else
+    {
+    System.out.println("consonant");
+    }
+      }
+      }
